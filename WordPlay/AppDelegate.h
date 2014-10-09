d1bc123907017aaecc760d8cbe2894370e881b9e
Selvan Ram <selvanprograms@gmail.com>
2014-10-08 18:49:23 -0700

    First Commit - Empty Files

---
+++ WordPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WordPlay
+//
+//  Created by Vijay Ram on 10/8/14.
+//  Copyright (c) 2014 Vijay R. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WordPlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WordPlay
+//
+//  Created by Vijay Ram on 10/8/14.
+//  Copyright (c) 2014 Vijay R. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
