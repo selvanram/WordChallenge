2b66cf53bde2d26058c9553c6ed7e280e238550f
Selvan Ram <selvanprograms@gmail.com>
2014-10-08 21:38:47 -0700

    2nd Version

---
+++ WordPlay/EnterAdjectiveViewController.h
@@ -0,0 +1,13 @@
+//
+//  EnterAdjectiveViewController.h
+//  WordPlay
+//
+//  Created by Vijay Ram on 10/8/14.
+//  Copyright (c) 2014 Vijay R. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EnterAdjectiveViewController : UIViewController
+
+@end
---
+++ WordPlay/ResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultsViewController.h
+//  WordPlay
+//
+//  Created by Vijay Ram on 10/8/14.
+//  Copyright (c) 2014 Vijay R. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultsViewController : UIViewController
+
+@end
